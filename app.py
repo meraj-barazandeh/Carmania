38b8dccd784b74a96b8e069a2f79a7c9ad5d5b44
Meraj-B <meraj.barazandeh@gmail.com>
2025-09-14 20:16:33 +0330

    feat: update app.py and auth routes

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
 from flask import Flask
+from database import db
+from routes.auth import auth_bp
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "hello from carmania"
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///carmania.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.secret_key = "super-secret-key"
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+
+    return app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
--- routes/auth.py
+++ routes/auth.py
@@ -4,3 +4,36 @@ from models import User
 from database import db
 
 auth_bp = Blueprint("auth", __name__)
+
+
+@auth_bp.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or not password:
+        return jsonify({"erorr": "username and password is required!"}), 400
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({"error": "username already exist"}), 400
+
+    hashed_password = generate_password_hash(password)
+    new_user = User(username=username, password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "user created successfully"}), 201
+
+
+@auth_bp.route("login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+
+    user = User.query.filter_by(username=username).first()
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({"error": "invalid username or password!"}), 401
+    return jsonify({"message": "login successful"}), 200
